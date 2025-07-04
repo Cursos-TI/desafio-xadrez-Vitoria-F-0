34e6c157b72aec10ecb8fb5c2ea3faa28016b090
Vitoria-F-0 <vitoriaflamarini09@gmail.com>
2025-07-04 00:23:11 +0000

    new commit

--- Desafio_Xadrez/Desafio_Xadrez.c
+++ Desafio_Xadrez/Desafio_Xadrez.c
@@ -33,5 +33,49 @@ int main() {
         printf("Esquerda\n"); //Esquerda uma vez
      }
     
+    //Recursividade para Torre, Rainha e loops aninhados para o Bispo
+
+    void mT (int casas) { //mT é mover Torre
+        if (casas > 0) {
+            printf("Direita\n");
+            mT (casas - 1);
+        }
+    }
+
+    mT(5);
+
+     void mR (int casas) { 
+        if (casas > 0) {
+            printf("Esquerda\n");
+            mR (casas - 1);
+        }
+    }
+
+    mR(8);
+
+    void mB(int movimentos) {
+    if (passos == 5) return;
+
+    for (int bC = 1; bC <= 1; bC++) {
+        for (int bD = 1; bD <= 1; bD++) {
+            printf("Cima Direita\n");
+        }
+    }
+    mB(movimentos + 1);
+}
+
+mB (0);
+
+    //Loops complexos para o Cavalo
+
+ for (int cC = 0, cD = 3; cD = 1 && cC < 3  ; cC++, cD--) {
+        if (cC == 0 || cC == 1) {
+            printf("Cima\n");
+        } 
+        if (cC == 2) {
+            printf("Direita\n");
+        }
+    }
+
     return 0;
 }
